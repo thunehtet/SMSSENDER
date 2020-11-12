bb1ee92ce88f9b0c32416f7ec845ae1ea1bc0359
SU MYAT SANDI AUNG <sumyatsandiaung1@gmail.com>
2020-11-13 00:23:40 +0800

    Add project files.

---
+++ SmsSender.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SMSSENDER
+{
+    public class SmsSender
+    {
+        int baudRate { get; set; }
+        int listenerPort { get; set; }
+        string comPort { get; set; }
+        public SmsSender(int ListenerPort, int BaudRate, string ComPort)
+        {
+            baudRate = BaudRate;
+            listenerPort = ListenerPort;
+            comPort = ComPort;
+
+        }
+        public int DefaultDatRetention { get; set; } = 720;
+
+        public async Task StartProcess()
+        {
+            UdpClient listener = new UdpClient(listenerPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenerPort);
+
+            try
+            {
+                while (true)
+                {
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    string incomeMessage = $" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}";
+                    await SendSms(baudRate, comPort, incomeMessage);
+                    await WriteLog("[RECEIVE] [IP :" + groupEP.Address + "][PORT :" + groupEP.Port + "][ Message :" + $" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}" + " ]");
+                    
+                }
+            }
+            catch (SocketException e)
+            {
+                await WriteLog("[RECEIVE] [ERROR] [IP :" + groupEP.Address + "][PORT :" + groupEP.Port + "] [Message:" + e.Message + "]");
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+      
+
+        private async Task SendSms(int BaudRate, string ComPort,string Message)
+        {
+            try
+            {
+
+                //"<OTP-" + Otp + ">|" + MobileNo + "|" + (smsDB.NoOfSms + 1));
+                //"<OTP-12345>|12345678|1";
+                string[] splitMessage = Message.Split("|");
+                string pattern = @"<OTP-(?<OTPNO>\d.*)>$";
+                var match = Regex.Match(splitMessage[0], pattern);
+                if (match.Success)
+                {
+                    string otpNo = match.Groups["OTPNO"].ToString();
+                    string moblieNo = splitMessage[1];
+                    string smsSerialNo = splitMessage[2];
+
+                    using SerialPort serialPort = new SerialPort(ComPort, BaudRate, Parity.None, 8, StopBits.One);
+
+                    try
+                    {
+                        serialPort.Open();
+                        serialPort.WriteLine(@"AT" + (char)(13));
+                        Thread.Sleep(200);
+                        serialPort.WriteLine("AT+CMGF=1" + (char)(13));
+                        Thread.Sleep(200);
+                        serialPort.WriteLine(@"AT+CMGS=""" + moblieNo + @"""" + (char)(13));
+                        Thread.Sleep(200);
+                        string smsMessage = "Your OTP is < " + otpNo + " >for your LTA PGS system log in account. #" + smsSerialNo + (char)(26);
+                        serialPort.WriteLine(smsMessage);
+                        Thread.Sleep(200);
+                        serialPort.Close();
+                        await WriteSmsSend("[ SEND SMS ] [SUCCESS ] [Mobile No :" + moblieNo + "][Message :" + smsMessage + "]");
+                    }
+                    catch (Exception ex)
+                    {
+                        await WriteSmsSend("[ SEND SMS ] [ ERROR ] " + ex.Message  );
+                    }
+
+                }
+                else
+                {
+                    await WriteLog("[ RECEIVE ]MESSAGE DISCARD-DO NOT NOT COMFORM FORMAT ::" + Message);
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                await WriteLog("[ SEND SMS ] [ ERROR ] "+e.Message);
+            }
+        }
+
+
+        public Task WriteLog(string logMessage)
+        {
+            Log("COM", logMessage, DefaultDatRetention);
+            return Task.CompletedTask;
+        }
+
+        public Task WriteSmsSend(string logMessage)
+        {
+            Log("SMS_SEND", logMessage, DefaultDatRetention);
+            return Task.CompletedTask;
+        }
+
+        private Task Log(string logName, string logMessage, int DataRetention)
+        {
+          try
+            {
+                string orPath = Path.GetDirectoryName(Environment.CurrentDirectory) + @"\SMSLOG";
+                string subDirecory = orPath + @"\" + logName;
+
+                if (!Directory.Exists(orPath))
+                {
+                    Directory.CreateDirectory(orPath);
+                }
+                if (!Directory.Exists(subDirecory))
+                {
+                    Directory.CreateDirectory(subDirecory);
+                }
+
+                string path = Path.Combine(subDirecory, logName).ToString() + DateTime.Now.ToString("yyyyMM") + ".log";
+
+                if (!File.Exists(path))
+                {
+                    File.Create(path).Dispose();
+                    StreamWriter sw = File.AppendText(path);
+                    sw.WriteLine("[ " + DateTime.Now.ToString("") + "]  Log is Started");
+                    sw.Close();
+                    DirectoryInfo yourRootDir = new DirectoryInfo(subDirecory);
+                    foreach (FileInfo file in yourRootDir.GetFiles())
+                        if (file.LastWriteTime < DateTime.Now.AddDays(-DataRetention))
+                            file.Delete();
+                }
+                else
+                {
+                    StreamWriter sw = File.AppendText(path);
+                    sw.WriteLineAsync("[ " + DateTime.Now.ToString("") + " ] [ " + logName + " LOG ] " + logMessage);
+                    sw.Close();
+                }
+            }
+            catch
+            {
+                return Task.CompletedTask;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SmsService.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SMSSENDER
+{
+    public class SmsService : BackgroundService
+    {
+        private readonly ILogger<SmsService> _logger;
+        private readonly IConfiguration _configuration;
+
+        public SmsService(ILogger<SmsService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                int baudRate = getBaudRate();
+                string comPort = getComPort();
+                int listenPort = messageListenerPort();
+                SmsSender sms = new SmsSender(listenPort,baudRate,comPort);
+                await sms.StartProcess();
+                
+            }
+        }
+
+        private string getComPort()
+        {
+            string com= _configuration.GetSection("SmsGateway").GetSection("COM").Value;
+            return com;
+        }
+
+        private int getBaudRate()
+        {
+            return Convert.ToInt32(_configuration.GetSection("SmsGateway").GetSection("BaudRate").Value);
+        }
+
+        private int messageListenerPort()
+        { 
+            return Convert.ToInt32(_configuration.GetSection("Message").GetSection("ListenPort").Value);
+        }
+    }
+}
